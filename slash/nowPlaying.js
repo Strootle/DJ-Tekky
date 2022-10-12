bb1195428f4ff159c9febf6914195af4f5682370
Jack Devey <jack@jw3.uk>
2022-10-12 14:12:45 +0100

    who knows if works

--- slash/nowPlaying.js
+++ slash/nowPlaying.js
@@ -3,11 +3,18 @@ const { SlashCommandBuilder, EmbedBuilder } = require("@discordjs/builders")
 module.exports = {
     data: new SlashCommandBuilder().setName("np").setDescription("Displays the song that is currently playing"),
     run: async ({ client, interaction }) => {
+        // Get the queue for the server
         const queue = client.player.getQueue(interaction.guildId)
 
-        if (!queue) return await interaction.editReply("There are no songs currently playing.")
-        let nowPlaying = new EmbedBuilder()
-    await interaction.editReply(`Currently playing: ${queue.nowPlaying.data}`),
-    console.log(queue.nowPlaying.data)
+        // Get song playing
+        let song = queue.nowPlaying
+
+        console.log(song)
+
+        // If no song playing
+        if(!song) return interaction.editReply("No songs rn playing u idiot .")
+
+        return interaction.editReply(`Name: ${song.name}`)
+
     },   
 }
