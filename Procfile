4cc04ddea98ac51259a8e1acf6f644c3489d2069
Harrison Ratcliffe <58830505+Strootle@users.noreply.github.com>
2022-10-12 20:55:39 +0100

    New Profile command
    Command must be ran when added to a new server, so the commands can be loaded to be used.

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+worker: node index.js load
 worker: node .
--- slash/stop.js
+++ slash/stop.js
@@ -1,7 +1,9 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
 
+
+//Stop commands which forces the queue to clear and DC the bot
 module.exports = {
-	data: new SlashCommandBuilder().setName("quit").setDescription("Stops the bot and clears the queue"),
+	data: new SlashCommandBuilder().setName("stop").setDescription("Stops the bot and clears the queue"),
 	run: async ({ client, interaction }) => {
 		const queue = client.player.getQueue(interaction.guildId)
 
